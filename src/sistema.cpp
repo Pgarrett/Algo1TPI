a69b9c84b66424c9e890ccdf7a158fed3874fb53
Philip Garrett <garrett.phg@gmail.com>
2016-05-30 02:14:26 -0300

    Mostrar y guardar de sistema

--- src/sistema.cpp
+++ src/sistema.cpp
@@ -58,10 +58,47 @@ void Sistema::volarYSensar(const Drone & d)
 
 void Sistema::mostrar(std::ostream & os) const
 {
+  os << "{ S ";
+  _campo.mostrar(os);
+  for(int i = 0; i < _enjambre.size(); i++)
+  {
+    _enjambre[i].mostrar(os);
+  }
+  os << " }";
 }
-
+//{ S { C [3,3] [[Cultivo,Cultivo,Granero], [Cultivo,Casa,Cultivo], [Cultivo, Cultivo,Cultivo]]} [{ D 12 83 [[1,2],[1,1],[1,0],[2,0]] [Plaguicida, PlaguicidaBajoConsumo, Herbicida, Fertilizante]}, { D 15 46 [[0,1],[1,1][2,1][2,2]] [HerbicidaLargoAlcance, Fertilizante, Herbicida, Plaguicida]}] [[NoSensado,EnCrecimiento,NoSensado], [ConMaleza,NoSensado,ConPlaga], [EnCrecimiento,ListoParaCosechar, ConPlaga]] }
 void Sistema::guardar(std::ostream & os) const
 {
+  os << "{ S ";
+  _campo.guardar(os);
+  os << " [";
+  for(int i = 0; i < _enjambre.size(); i++)
+  {
+    _enjambre[i].mostrar(os);
+    if (i < _enjambre.size() - 1)
+      os << ",";
+  }
+  os << "]";
+  Dimension d = _campo.dimensiones();
+  for(int i=0; i<d.ancho; i++)
+  {
+    os << "[";
+    Posicion p1 = Posicion();
+    p1.x = i;
+    p1.y = 0;
+    os << _campo.contenido(p1);
+    for(int j=1; j<d.largo; j++)
+    {
+      Posicion p2 = Posicion();
+      p2.x = i;
+      p2.y = 0;
+      os << "," << _campo.contenido(p2);
+    }
+
+    if(i<d.ancho-1)
+      os << "], ";
+  }
+  os << "]]}";
 }
 
 void Sistema::cargar(std::istream & is)

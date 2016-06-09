04231e5f92f8e97072209b6885a9502b63d76fa2
Rocio Belfiore <rociobelfiore@gmail.com>
2016-06-09 13:55:33 -0300

    Removed custom test.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 
-#include "test.cpp"
-
 int main()
 {
-  //campo_test();
-  std::cout << endl << endl;
-  //drone_test();
-  std::cout << endl << endl;
-  sistema_test();
   return 0;
 }
